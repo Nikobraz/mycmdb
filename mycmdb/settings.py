42b38db084d5035812724eed64fa5d7d5c688cc5
Nikobraz <nikita_yarushin@mail.ru>
2020-01-22 15:21:19 +0300

    Update settings.py

--- mycmdb/settings.py
+++ mycmdb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(uc((k)l=+a^@=2bha0dbo#a@1yx+d@kj3kenwf1dsbwgf12&d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
