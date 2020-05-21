da7761ba5d08756db563d42596c85946ee1d24b6
Nikobraz <nikita_yarushin@mail.ru>
2020-05-21 11:05:03 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mycmdb',
+      version='0.1',
+      description='Sample CMDB',
+      url='https://github.com/Nikobraz/mycmdb',
+      author='Flying Circus',
+      author_email='nikita_yarushin@mail.ru',
+      license='MIT',
+      packages=['mycmdb'],
+      zip_safe=False)
